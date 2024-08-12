5f244d7bc19c62a2c53e4ef23b311178c789353f
config.json <configjson@proton.me>
2024-08-13 00:13:10 +0300

    fix: resolve import

--- examples/index.ts
+++ examples/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 import { PublicKey } from '@solana/web3.js'
-import SolanaMirror from 'solana-mirror'
+import SolanaMirror from '../src/SolanaMirror'
 
 const TEST_ACCOUNT = new PublicKey(
     'GhCar5JLrUencisZDBLPFsWiWQs5qfimejpU5wjzgS8y'
